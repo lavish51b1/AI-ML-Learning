1825ad2d8be5dcc074a2a95fa567ded6225dfed1
lavish51b1 <lavishgoyal0209@gmail.com>
2025-12-19 11:05:28 +0530

    Learned conditionals & practiced some questions

---
+++ Python/Conditionals/Q_Practice.py
@@ -0,0 +1,79 @@
+# Q1. WAP to grade students based on marks; marks>=90,grade ="A" ;90>marks>=80,grade = "B" ;80>marks>=70,grade = "C" ; 70>marks,  grade = "D".
+
+marks = float(input("Enter your marks: "))
+
+if(marks>=90):
+    print("Grade A")
+elif(90>marks>=80):
+    print("Grade B")
+elif(80>marks>=70):
+    print("Grade C")
+else:
+    print("Grade D")
+
+
+# Q2. WAP to check if a number entered by the user is odd or even.
+
+num = int(input("Enter a number: "))
+
+if(num%2 == 0):
+    print("Even")
+else:
+    print("Odd")
+
+
+# Q3. WAP to find greatest of 3 numbers entered by user.
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+num3 = int(input("Third number: "))
+
+if(num1 >= num2 and num1 >= num2):
+    num = num1
+elif(num2 >= num1 and num2 >= num3):
+    num = num2
+else:
+    num = num3
+
+print("The greatest among these three is:",num)
+"""
+
+# Q4. WAP to check if a number is a multiple of 7 or not.
+
+num = int(input("Enter a number: "))
+
+if(num % 7 == 0):
+    print("Entered number is multiple of 7")
+else:
+    print("Not a multiple of 7")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Python/Conditionals/README.md
@@ -0,0 +1,44 @@
+# Python Conditionals (Syntax Only)
+
+## if – else
+
+if condition:
+    pass
+else:
+    pass
+
+## if – elif – else
+
+if condition1:
+    pass
+elif condition2:
+    pass
+elif condition3:
+    pass
+else:
+    pass
+
+## Nested if
+
+if condition1:
+    if condition2:
+        pass
+    else:
+        pass
+else:
+    pass
+
+## Nested if with elif
+
+if condition1:
+    if condition2:
+        pass
+    elif condition3:
+        pass
+    else:
+        pass
+else:
+    if condition4:
+        pass
+    else:
+        pass
